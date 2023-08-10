ae058cdfeabab8ae49251a0e9eb7884028d96591
jiminghe <89671976+jiminghe@users.noreply.github.com>
2023-08-10 10:05:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main()
 {
     try
     {
-        SerialHandler serial("/dev/ttyUSB0", LibSerial::BaudRate::BAUD_115200);
+        SerialHandler serial("/dev/ttyUSB0", LibSerial::BaudRate::BAUD_115200); //Change this to your port name and baudrate(which could be configured at MT Manager - Device Settings)
         XbusPacket packet;
 
         std::vector<uint8_t> goToConfig = {0xFA, 0xFF, 0x30, 0x00};
